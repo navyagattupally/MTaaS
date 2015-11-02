10a664346ef7c6618c5c0cb1da31bba95c838fc3
KokilAwasthi <kokilmishra@gmail.com>
2015-11-01 19:21:19 -0800

    Title updated

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -2,9 +2,9 @@
 /*
  * GET home page.
  */
-
+/* Tilte Updated by Kokil*/
 exports.index = function(req, res){
-  res.render('index', { title: 'CMPE 281 HW 1 Cloud Load Balancer - TEAM 21'});
+  res.render('index', { title: 'MTaaS Cloud Load Balancer'});
 };
 
 exports.ping = function(req, res){
