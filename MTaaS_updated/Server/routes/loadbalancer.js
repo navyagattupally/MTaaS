4d0f7c9d580099f0b60f68a6f27edf14dba2bd56
navya <navyagattupally@ymail.com>
2015-11-23 18:36:38 -0800

    Bug Fix

--- MTaaS_updated/Server/routes/loadbalancer.js
+++ MTaaS_updated/Server/routes/loadbalancer.js
@@ -8,6 +8,9 @@ var conf = (JSON.parse(fs.readFileSync("./config/conf.json", "utf8")));
 var random = require("random-js")(); // uses the nativeMath engine
 var value = Math.floor(Math.random() * (5 ) + 0);
 var vms = ["1.0.0.0","2.0.0.0","3.0.0.0","4.0.0.0","5.0.0.0"];
+//commented by Kokil , to pick algo from UI rather than conf file.
+//var loadBal=conf.loadBalanceAlgo;
+//var algo = require("../loadbalancers/"+loadBal);
 
 var nodemailer = require('nodemailer');
 
@@ -59,3 +62,101 @@ exports.resourceip = function(req, res){
 console.log("chosen vm : " + value);
 return res.send(vms[value]);	
 }
+
+
+exports.resourceRequest = function(req, res){
+		
+	if (req.body.hasOwnProperty('algo'))
+	{
+		console.log(" ********algo is : *********" + req.param('algo'));
+	}
+
+	var loadBal=req.param('algo');
+	var algo = require("../loadbalancers/"+loadBal);
+	
+	
+	
+	/** for(var i = 0 ; i< conf.loadBalanceAlgo.AlgoType.length ; i++)
+	 {
+	   if ( (req.param('algo')) == conf.loadBalanceAlgo.AlgoType[i]) 
+	   {
+	   loadBal = req.param('algo');
+	   break;
+	   }
+	 }**/
+	
+		if(conf.role == "loadbalancer")
+		  {
+		  	algo.allocateServer(function(allocatedServer,err){
+		  		if(err)
+					console.log(err);
+				else
+					{
+					console.log(req.body);
+					console.log(allocatedServer);
+					for(node in conf.server.serverNodes)
+					  {
+					  	var server=conf.server.serverNodes[node];
+					  	if(server.nodeId==allocatedServer)
+					  		{
+					  			url="http://" + server.host + ":" + server.port + req.path;
+					  			/**
+					  			 *------- TO DO ---------
+					  			 * 1) Ping the server to see if its running
+					  			 * 2) Only in the case its running rediret the request
+					  			 * 3) Call the loadbalancer again to allocate new server
+					  			 *    incase the one allocated is not running
+					  			 */
+					  			console.log(url);
+					  			res.location(url);
+					  			return res.redirect(307, url);
+					  		}
+					  }
+					}
+		  	},conf,req);
+		  }
+	  else if (conf.role== "server")
+		  {
+		  	console.log('role : server');
+		  	if(req.body.hasOwnProperty('quantity') && req.body.hasOwnProperty('duration') && req.body.hasOwnProperty('mobile_os') && req.body.hasOwnProperty('ram') && req.body.hasOwnProperty('disk') && req.body.hasOwnProperty('CPU')) 
+		  	{
+			  if(conf.loadBalanceAlgo=="ant")
+			  {
+				  var slength=conf.server.serverNodes.length;
+				  var prize=function random(slength) 
+				  {
+					  var x = Math.cos(slength) * 10000;
+					  return x - Math.floor(x);
+				  }
+				  incrementPheromoneBySid(conf.nodeId,prize);
+			  }
+			  /**
+	  			 *------- TO DO ---------
+	  			 * 1) Each parameter has an associated cost
+	  			 * 2) Store and retrieve cost for each parameter in the DB (eg 1 ghz cpu -> 5 $, 2 gb RAM -> $ 10 etc
+	  			 * 3) Limited resources, so maintain resource count in DB and decline requests when resources not available
+	  			 * 4) Compute and return cost according to resources requested.
+	  			 */
+				console.log("****** Request For Resource recieved with the following Configuration");
+				console.log("** CPU --> " + req.body.CPU  + "**");
+				console.log("** DISK --> " + req.body.disk  + "**");
+				console.log("** RAM --> " + req.body.ram  + "**");
+				console.log("** MOBILE OS --> " + req.body.mobile_os  + "**");
+				console.log("** DURATION --> " + req.body.duration  + "**");
+				console.log("** QUANTITY --> " + req.body.quantity  + "**");
+				console.log("** LATITUDE --> " + req.body.latitude  + "**");
+				console.log("** LONGITUDE --> " + req.body.longitude  + "**");
+				
+				console.log("\n\n **##### TOTAL COMPUTED COST -->> $50.00  #####**");
+				return res.send('Total Cost : $50');
+				
+			}
+		  else
+		  	{
+		  		console.log('Has no parameters');
+		  		console.log(req.body);
+		  		return res.send('No post parameters undi');
+		  	}
+		  }
+	};
+
