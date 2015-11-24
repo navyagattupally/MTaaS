fd7c37bd470b6c5b4121e9afb32113d25bf81b1f
navyagattupally <navyagattupally@ymail.com>
2015-11-24 11:37:11 -0800

    Clean up

--- MTaaS_updated/Server/routes/loadbalancer.js
+++ MTaaS_updated/Server/routes/loadbalancer.js
@@ -69,7 +69,7 @@ exports.resourcelist = function(req, res){
 	
 var random = require("random-js")(); // uses the nativeMath engine
 var value = Math.floor(Math.random() * (5 ) + 0);
-var vms = [{"Public IP":"54.213.242.182","Region":"N.California","Memory":"256GB","VMType":"Ubuntu","State":"Running"},{"IP":"54.213.238.146","Region":"Oregon","Memory":"500GB","VMType":"Linux","State":"Running"},{"IP":"54.213.238.127","Region":"N.California","Memory":"512GB","VMType":"Ubuntu","State":"Running"},{"IP":"54.213.242.182","Region":"Oregon","Memory":"256GB","VMType":"Ubuntu","State":"Running"},{"IP":"54.213.252.190","Region":"Virginia","Memory":"512GB","VMType":"Linux","State":"Running"}];
+var vms = [{"IP":"54.213.242.182","Region":"N.California","Memory":"256GB","VMType":"Ubuntu","State":"Running"},{"IP":"54.213.238.146","Region":"Oregon","Memory":"500GB","VMType":"Linux","State":"Running"},{"IP":"54.213.238.127","Region":"N.California","Memory":"512GB","VMType":"Ubuntu","State":"Running"},{"IP":"54.213.242.182","Region":"Oregon","Memory":"256GB","VMType":"Ubuntu","State":"Running"},{"IP":"54.213.252.190","Region":"Virginia","Memory":"512GB","VMType":"Linux","State":"Running"}];
 console.log("chosen vm : " + vms);
 return res.send(vms);
 }
@@ -166,7 +166,7 @@ exports.resourceRequest = function(req, res){
 		  	{
 		  		console.log('Has no parameters');
 		  		console.log(req.body);
-		  		return res.send('No post parameters undi');
+		  		return res.send('No post parameters');
 		  	}
 		  }
 	};
